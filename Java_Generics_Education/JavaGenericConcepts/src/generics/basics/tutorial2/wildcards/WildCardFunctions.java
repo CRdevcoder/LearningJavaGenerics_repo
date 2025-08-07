130ca2071c9278307e19c72da27cb896087c946a
CRdevcoder <chris.rodriguez8123@gmail.com>
2025-08-07 15:51:11 -0400

    Created WildCardFunctions class

---
+++ Java_Generics_Education/JavaGenericConcepts/src/generics/basics/tutorial2/wildcards/WildCardFunctions.java
@@ -0,0 +1,16 @@
+package generics.basics.tutorial2.wildcards;
+
+public class WildCardFunctions {
+    public static <T> void displayToString(T typeData)
+    {
+        System.out.println();
+    }
+
+
+    // Lower Bound Wildcard
+
+    // Unbound Wildcard
+
+    // Upper Bound Wildcard
+
+}
