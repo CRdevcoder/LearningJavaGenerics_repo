5d5d74f69d542fa8b417da1bc6de6087a847c31e
CRdevcoder <chris.rodriguez8123@gmail.com>
2025-08-20 00:11:48 -0400

    NEW smallprojects package: Contains new notepad project, will handle text files.

---
+++ Java_Generics_Education/JavaGenericConcepts/src/smallprojects/notepad/NotePadMain.java
@@ -0,0 +1,11 @@
+package smallprojects.notepad;
+
+import smallprojects.notepad.gui.AppWindow;
+
+public class NotePadMain {
+
+    public static void main(String[] args) {
+        AppWindow app = new AppWindow("Java Text NotePad");
+        app.setVisible(true);
+    }
+}
---
+++ Java_Generics_Education/JavaGenericConcepts/src/smallprojects/notepad/gui/AppWindow.java
@@ -0,0 +1,93 @@
+package smallprojects.notepad.gui;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class AppWindow extends JFrame implements ActionListener{
+
+    //Components:
+    JFileChooser chooser;
+    JButton saveButton;
+    JButton selectFileButton;
+    JButton saveAsButton;
+
+    // text area
+    JTextArea notepadArea;
+
+    // Jpanels:
+    JPanel buttonPanel;
+    JPanel notePanel;
+
+    // window for notepad app.
+    public AppWindow(String title)
+    {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //this.setLayout(new BoxLayout(null,BoxLayout.Y_AXIS)); // layout
+        this.setLayout(new FlowLayout());
+
+        this.setUpComponents();
+
+        this.pack();
+        
+    }
+
+    private void setUpComponents()
+    {
+        // button set up.
+        saveButton = new JButton("SAVE");
+        saveAsButton = new JButton("SAVE AS");
+        selectFileButton = new JButton("Open File");
+        // text Area.
+        notepadArea = new JTextArea(30,40);
+
+        // this window class handles all action events.
+        saveButton.addActionListener(this);
+        saveAsButton.addActionListener(this);
+        selectFileButton.addActionListener(this);
+        
+        // panels
+        buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        notePanel = new JPanel();
+
+        // adding to button panel.
+        buttonPanel.add(saveButton);
+        buttonPanel.add(saveAsButton);
+        buttonPanel.add(selectFileButton);
+        // adding to note panel.
+        notePanel.add(notepadArea);
+        // adding to window.
+        this.add(buttonPanel);
+        this.add(notePanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource().equals(selectFileButton))
+        {
+            chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new File("notepadAppStorage\\"));
+
+            int response = chooser.showOpenDialog(null); // select file to open.
+
+
+            if( response == JFileChooser.APPROVE_OPTION)
+            {
+                File file = new File(chooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+            }
+        }
+        
+    }
+
+}
