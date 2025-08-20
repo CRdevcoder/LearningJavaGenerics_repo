619ef9dd659393b156ab79fba7e0de73cd7aed63
CRdevcoder <chris.rodriguez8123@gmail.com>
2025-08-19 20:16:15 -0400

    Finished first Csv file tutorial

--- Java_Generics_Education/JavaGenericConcepts/src/filehandling/Main.java
+++ Java_Generics_Education/JavaGenericConcepts/src/filehandling/Main.java
@@ -2,20 +2,60 @@ package filehandling;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+// tutorial:
+// https://www.youtube.com/watch?v=zKDmzKaAQro
+// not exact copy, I took my own liberties
 public class Main {
 
     public static void main(String[] args) {
         
         // Handle CSV files.
-        String file = "csvFile/exampleData.csv";
+        String file = "csvFiles\\exampleData.csv";
         BufferedReader reader = null;
         String line = "";
 
+        // stores rows of csv file
+        ArrayList<String[]> tableRows = new ArrayList<>();
+
+        // try opening file
         try{
             reader = new BufferedReader(new FileReader(file));
-            
+            // read lines in file, stop when next line is null.
+            while((line = reader.readLine()) != null)
+            {
+                String[] row = line.split(",");
+                // add to arraylist
+                tableRows.add(row);
+            }
+
         }
+        catch( Exception e)
+        {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        finally
+        {
+            // close reader
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // print out data
+        for (String[] row : tableRows) {
+            
+            for (String index : row) {
+                System.out.printf("%-14s",index);
+            }
+            System.out.println();
+        }
+
 
     }
 
