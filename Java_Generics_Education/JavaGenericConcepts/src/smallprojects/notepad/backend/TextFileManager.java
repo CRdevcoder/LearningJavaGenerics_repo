b9862ccf41be31584b3b22e120ddb6d547b76227
CRdevcoder <chris.rodriguez8123@gmail.com>
2025-08-25 12:55:12 -0400

    New TextFileManager class for backend of notepad app.

---
+++ Java_Generics_Education/JavaGenericConcepts/src/smallprojects/notepad/backend/TextFileManager.java
@@ -0,0 +1,15 @@
+package smallprojects.notepad.backend;
+
+// Backend for notepad project.
+// Handle text files by:
+//      saving data to file
+
+public class TextFileManager {
+
+    // Method to save to file.
+
+    // Method to open text file.
+
+    // Method to check if file path leads to text file.
+
+}
