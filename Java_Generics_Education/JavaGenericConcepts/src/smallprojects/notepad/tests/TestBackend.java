8ece127987b015923da3666c5d77521d93d25433
CRdevcoder <chris.rodriguez8123@gmail.com>
2025-08-27 18:17:21 -0400

    Update: TestBackend class Testing TextFileManager writeStringToFile and readFiletoStringArrayList methods. Made console output more readable, added more descriptive output prints. Improved comments.

--- Java_Generics_Education/JavaGenericConcepts/src/smallprojects/notepad/tests/TestBackend.java
+++ Java_Generics_Education/JavaGenericConcepts/src/smallprojects/notepad/tests/TestBackend.java
@@ -1,5 +1,6 @@
 package smallprojects.notepad.tests;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -7,6 +8,7 @@ import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.SortedMap;
 
@@ -14,7 +16,7 @@ import filehandling.util.ScannerUtil;
 import smallprojects.notepad.backend.TextFileManager;
 
 /* tests backend classes:
-*  TextFileManager classes
+*  TextFileManager class
 *  Other:
 *  Utilizes Temporary files with Files.createTempFile method.
 */
@@ -27,6 +29,8 @@ public class TestBackend {
         
         Path testPath = Paths.get(".\\textFiles\\tut2output.txt");
 
+        // Testing isTextFile and isValidFile methods.
+        System.out.println("Testing isTextFile and isValidFile methods:\nUsing file:" + testPath);
         boolean isTxt = TextFileManager.isTextFile(testPath);
         System.out.println("Is text file: " + isTxt);
         boolean isValidFile = TextFileManager.isValidFile(testPath);
@@ -36,18 +40,24 @@ public class TestBackend {
         Path fileFolder = Paths.get(".","textFiles");
         Path tempPath, tempInvalid;
 
-        System.out.println("Creating temp text file in: " + fileFolder);
+        System.out.println("\nCreating temp text file in: " + fileFolder);
         
         // using createTempFile
+        // Testing TextFileManager constructor and isTextFile method
         try {
             tempPath = Files.createTempFile(fileFolder, "testTemp",".txt");
             tempInvalid = Files.createTempFile(fileFolder, "failTemp",".html");
             TextFileManager bestMan = new TextFileManager(tempPath);
 
-            BufferedWriter bestWriter = bestMan.writeStringToFile("This is a temp file\nGood day!", StandardOpenOption.WRITE);
-            bestWriter.close();
+
+            // testing isTextFile method using Path objects.
+            System.out.println("\nTesting isTextFile method with html file directory:");
+            System.out.println("Is file:" + tempPath + "\nA text file?: " + TextFileManager.isTextFile(tempPath));
+            System.out.println("Is file:" + tempInvalid + "\nA text file?: " + TextFileManager.isTextFile(tempInvalid));
+
 
             // testing constructor exception handling.
+            System.out.println("\nTesting Contructor Exception handling (when given NON .txt file):");
             try {
                 TextFileManager failMan = new TextFileManager(tempInvalid);
             } catch (Exception e) {
@@ -56,10 +66,11 @@ public class TestBackend {
             finally
             {
                 // delete temp file.
-                Files.deleteIfExists(tempInvalid);;
+                Files.deleteIfExists(tempInvalid);
             }
 
-            ScannerUtil.askYesNoQuestionInput(new Scanner(System.in),"Enter response to continue program");
+            // pause to observe files being created in IDE file directory.
+            //ScannerUtil.askYesNoQuestionInput(new Scanner(System.in),"Enter response to continue program");
 
             // delete temporary files
             Files.deleteIfExists(tempPath);
@@ -68,11 +79,27 @@ public class TestBackend {
             System.out.println(e.getMessage());
         }
 
-        // Different Charsets
-        System.out.println("\nSet of available Charsets:");
-        SortedMap<String,Charset> setMap = Charset.availableCharsets();
-        System.out.println(setMap.keySet());
+        //Testing Writing to file and reading to file.
+        System.out.println("\nTesting Writing to file and Reading file:");
 
+        try {
+            tempPath = Files.createTempFile(fileFolder, "sampleTemp",".txt");
+            TextFileManager sampleManager = new TextFileManager(tempPath);
+            // Writing to file.
+            BufferedWriter smWriter = sampleManager.writeStringToFile("This is a sample file\nGood day!",Charset.forName("US-ASCII"), StandardOpenOption.WRITE);
+            smWriter.close(); // closing writer.
+
+            // Reading file
+            ArrayList<String> textList = sampleManager.readFiletoStringArrayList(Charset.forName("US-ASCII"));
+            System.out.println("Contents of file: " + tempPath + "\n" + textList);
+            
+            // deleting temp file.
+            Files.deleteIfExists(tempPath);
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
         
     }
 
